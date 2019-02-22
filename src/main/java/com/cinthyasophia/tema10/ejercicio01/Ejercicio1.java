b1843fc0e3c3805127c6d7974381473c351dd235
cinthya <cinthysophia@gmail.com>
2019-02-22 19:32:00 +0100

    ejercicio 1

--- src/main/java/com/cinthyasophia/tema10/ejercicio01/Ejercicio1.java
+++ src/main/java/com/cinthyasophia/tema10/ejercicio01/Ejercicio1.java
@@ -15,31 +15,35 @@ public class Ejercicio1 {
             array[i]=aleatorio;
         }
         lista=listaEnteros(array);
-        System.out.println();
+        System.out.println(lista);
 
     }
 
     public ArrayList<Integer> listaEnteros(int[] array){
 
         ArrayList<Integer> lista=new ArrayList<>(array.length);
-        int punteroPar=0;
-        int punteroImpar= lista.size();
 
-        do {
+        for (int i = 0; i <array.length ; i++) {
+            lista.add(i,0);
+        }
+        int punteroPar=0;
+        int punteroImpar= lista.size()-1;
+
+
             for (int i = 0; i <array.length ; i++) {
 
                 if (array[i]%2==0){
-                    lista.add(punteroPar,array[i]);
+                    lista.set(punteroPar,array[i]);
                     punteroPar++;
 
                 }else {
-                    lista.add(punteroImpar,array[i]);
-                    //punteroImpar--;
+                    lista.set(punteroImpar,array[i]);
+                    punteroImpar--;
                 }
-                //lista.
+
             }
 
-        }while(punteroPar!=punteroImpar);
+
 
         return lista;
 
