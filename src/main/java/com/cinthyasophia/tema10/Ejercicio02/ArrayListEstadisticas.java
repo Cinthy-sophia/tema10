6601e896fc37b57f3895a76ee77e19342224e241
cinthya <cinthysophia@gmail.com>
2019-02-23 23:22:41 +0100

    Añado carpeta util

--- src/main/java/com/cinthyasophia/tema10/Ejercicio02/ArrayListEstadisticas.java
+++ src/main/java/com/cinthyasophia/tema10/Ejercicio02/ArrayListEstadisticas.java
@@ -1,24 +1,27 @@
 package com.cinthyasophia.tema10.Ejercicio02;
 
+
 import java.util.ArrayList;
 import java.util.HashMap;
+
+
 
 public class ArrayListEstadisticas implements IEstadisticas {
     private ArrayList<Double> lista;
 
     public ArrayListEstadisticas(){
         lista = new ArrayList<>();
+
     }
 
     @Override
     public double minimo() {
         double min;
         min= Double.MAX_VALUE;
-        for (int i = 0; i < lista.size(); i++) {
-            if (lista.get(i)<min) {
-                min=lista.get(i);
+        for (Double num : lista) {
+            if (num < min) {
+                min = num;
             }
-
         }
         return min;
     }
@@ -27,11 +30,10 @@ public class ArrayListEstadisticas implements IEstadisticas {
     public double maximo() {
         double max;
         max=Double.MIN_VALUE;
-        for (int i = 0; i < lista.size(); i++) {
-            if (lista.get(i)>max) {
-                max=lista.get(i);
+        for (Double num : lista) {
+            if (num > max) {
+                max = num;
             }
-
         }
         return max;
     }
@@ -58,11 +60,68 @@ public class ArrayListEstadisticas implements IEstadisticas {
 
     @Override
     public double moda() {
-        HashMap<Double,Integer> repetidos = new HashMap<>();
+        HashMap<Double,Double> repetidos = new HashMap<>();
+        double contadorRep;
+        double max= Double.MIN_VALUE;
+        ArrayList<Double> claves = new ArrayList<>();
 
         for (int i = 0; i <lista.size() ; i++) {
+            if(!repetidos.containsKey(lista.get(i))) {
+
+                for (Double num : lista) {
+                    contadorRep = 0;
+                    if (lista.get(i).equals(num)) {
+                        contadorRep++;
+                    }
+                    repetidos.put(lista.get(i), contadorRep);
+                    claves.add(lista.get(i));
+                }
+            }
+        }
+
+        for (double num : lista) {
+
+            if (repetidos.get(num) > max) {
+                max = repetidos.get(num);
+            }
 
         }
-        return 0;
+        return max;
+    }
+
+
+
+    public int size() {
+        return lista.size();
+    }
+
+
+    public boolean isEmpty() {
+        return lista.isEmpty();
+    }
+
+
+    public boolean contains(double o) {
+        return lista.contains(o);
+    }
+
+
+    public Object get(int i) {
+        return lista.get(i);
+    }
+
+
+    public Object set(int i, double o) {
+        return lista.set(i,o) ;
+    }
+
+
+    public Object remove(int i) {
+        return lista.remove(i);
+    }
+
+    public void add(int i, double o) {
+        lista.add(i,o);
+
     }
 }
--- src/main/java/com/cinthyasophia/tema10/Ejercicio02/Ejercicio2.java
+++ src/main/java/com/cinthyasophia/tema10/Ejercicio02/Ejercicio2.java
@@ -2,4 +2,10 @@ package com.cinthyasophia.tema10.Ejercicio02;
 
 public class Ejercicio2 {
 
+    public Ejercicio2(){
+        ArrayListEstadisticas estadisticas = new ArrayListEstadisticas();
+
+    }
+
+
 }
