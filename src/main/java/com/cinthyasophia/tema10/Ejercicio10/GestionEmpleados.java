026a3cba0460ed9d477dda8949a4b5505ecf716b
cinthya <cinthysophia@gmail.com>
2019-03-01 13:56:04 +0100

    ejercicio 5,6 terminado y ejercicio 10 iniciado

--- src/main/java/com/cinthyasophia/tema10/Ejercicio10/GestionEmpleados.java
+++ src/main/java/com/cinthyasophia/tema10/Ejercicio10/GestionEmpleados.java
@@ -1,10 +1,15 @@
 package com.cinthyasophia.tema10.Ejercicio10;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GestionEmpleados {
     public Scanner lector = new Scanner(System.in);
+
+    private ArrayList<String> Empleados;
+
 
     public int menuPrincipal(){
         int opcion=0;
@@ -39,4 +44,37 @@ public class GestionEmpleados {
         return opcion;
 
     }
+    public int menuConsultas(){
+        int opcion=0;
+        boolean isNumber;
+        System.out.println("*******************");
+        System.out.println("*CONSULTAS EMPLEADOS*");
+        System.out.println("*******************");
+        System.out.println("1. Buscar por NIF.");
+        System.out.println("2. Buscar por nombre.");
+        System.out.println("3. Buscar por rango de edad.");
+        System.out.println("4. Buscar por rango de sueldo.");
+        System.out.println("5. Buscar por hijos menores de edad.");
+        System.out.println("---------------------");
+        System.out.println("0. Volver al menu principal.");
+        do {
+            try{
+                opcion=lector.nextInt();
+                isNumber=true;
+            }catch (InputMismatchException ime){
+                System.out.println("Debes introducir numeros, no letras.");
+                isNumber=false;
+            }finally {
+                lector.nextLine();
+            }
+            if (opcion<0||opcion>5){
+                System.out.println("Opcion incorrecta.");
+            }
+
+        }while(!isNumber||opcion<0||opcion>5);
+
+        return opcion;
+
+    }
+
 }
